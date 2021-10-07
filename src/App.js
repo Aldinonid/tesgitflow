67cf4299e1b44619d92b2aff5272498a3168b838
Aldinonid <aldino.efendi7@gmail.com>
2021-10-07 11:16:27 +0700

    Add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Footer from "./footer";
+import Header from "./header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello World</h1>
       <Footer />
     </div>
